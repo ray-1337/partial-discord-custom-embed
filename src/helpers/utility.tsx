977f522301e4d115617b656a7847df78f06bd4d8
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-02-29 06:25:00 +0100

    replace all previous functions and add a function to truncate strings

--- src/helpers/utility.tsx
+++ src/helpers/utility.tsx
@@ -1,12 +1,3 @@
-import { validateID } from "ytdl-core";
-import { ServerSidePropsWithV } from "./typings";
-
-export function dynamicSearchForYouTubeID(context: ServerSidePropsWithV) {
-  if (context?.query?.ytID === "watch" && context?.query?.v?.length && validateID(context.query.v as string)) {
-    return String(context.query.v);
-  } else if (validateID(context?.query?.ytID as string)) {
-    return String(context.query.ytID);
-  };
-
-  return null;
+export function truncateString(str: string, limit: number) {
+  return str.length > limit ? str.slice(0, limit) + "..." : str;
 };
